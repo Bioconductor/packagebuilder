507a9bf79b5b5790c4d17c02793663f921a9e351
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-01-27 18:07:31 +0000

    fix syntax error
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioC/admin/build/packagebuilder@52131 bc3139a8-67e5-0310-9ffc-ced21a209358

--- workers/builder.py
+++ workers/builder.py
@@ -181,7 +181,7 @@ out_fh = open(outfile, "w")
 start_time = datetime.datetime.now()
 thread.start_new(tail,(outfile,))
 p = subprocess.Popen(os.getenv("BBS_R_CMD") + " CMD BUILD " + flags + " " + export_path, stdout=out_fh,
-    stderr=subprocess.STDOUT, shell=Tr)
+    stderr=subprocess.STDOUT)
 sts = os.waitpid(p.pid, 0)[1]
 stop_time = datetime.datetime.now()
 elapsed_time = str(stop_time - start_time)
