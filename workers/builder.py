dc41ddd401fb54831289be9b5c4ed32ffeff0371
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-02-01 02:34:09 +0000

    debugging....
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioC/admin/build/packagebuilder@52294 bc3139a8-67e5-0310-9ffc-ced21a209358

--- workers/builder.py
+++ workers/builder.py
@@ -283,7 +283,11 @@ def propagate_package():
     
     files_to_delete = "%s/%s_*.%s" % (repos, package_name, ext)
     
-    retcode = ssh("rm -f %s" % files_to_delete) #todo abort build if retcode != 0
+    if (platform.system() == "Windows"):
+        retcode = subprocess.call("c:/cygwin/bin/ssh.exe -qi e:/packagebuilder/.packagebuilder.private_key.rsa -o StrictHostKeyChecking=no biocadmin@merlot2 'rm -f /loc/www/bioconductor-test.fhcrc.org/course-packages/bin/windows/contrib/2.12/%s_*.zip'" % package_name)
+    else:
+        retcode = ssh("rm -f %s" % files_to_delete) #todo abort build if retcode != 0
+
     print("result of deleting files: %d" % retcode)
     send_message({"body": "pruned repos with retcode %d" % retcode, "status": "pruned_repos_retcode", "retcode": retcode})
     
