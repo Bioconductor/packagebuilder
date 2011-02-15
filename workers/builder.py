cc32e41b1967944fa897b87219a3bbdf5a3d1e22
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-02-15 23:37:58 +0000

    more feedback and robustness about svn export
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioC/admin/build/packagebuilder@52762 bc3139a8-67e5-0310-9ffc-ced21a209358

--- workers/builder.py
+++ workers/builder.py
@@ -218,9 +218,13 @@ def svn_export():
         os.getenv("SVN_USER"), os.getenv("SVN_PASS"), manifest['svn_url'], package_name)
     clean_svn_cmd = svn_cmd.replace(os.getenv("SVN_USER"),"xxx").replace(os.getenv("SVN_PASS"),"xxx")
     send_message({"status": "svn_cmd", "body": clean_svn_cmd})
+    send_message({"status": "post_processing", retcode: 0, "body": "starting svn export"})
     retcode = subprocess.call(svn_cmd, shell=True)
+    send_message({"status": "post_processing", retcode: retcode, "body": "finished svn export"})
     send_message({"status": "svn_result", "result": retcode, "body": \
         "svn export completed with status %d" % retcode})
+    if (not retcode == 0):
+        sys.exit("svn export failed")
     
 def build_package():
     global stop_thread
