36b900654c9f1eb7ad129ab1659a627bf48e96c9
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-11-17 02:18:44 +0000

    incremental checkin
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioC/admin/build/packagebuilder@60427 bc3139a8-67e5-0310-9ffc-ced21a209358

--- workers/installPkgDeps.R
+++ workers/installPkgDeps.R
@@ -11,23 +11,73 @@ if (length(args) == 0) {
 s <- paste(args, collapse=" ")
 segs <- strsplit(s, ";", fixed=TRUE)
 
+r <- paste(segs[[1]][1:length(segs[[1]])-1])
 
 
-for (i in 1:length(args)) {
-    eval(parse(text=args[i]))
+for (i in 1:length(r)) {
+    eval(parse(text=r[i]))
 }
 
-fields <- c("Depends", "Imports", "Suggests", "Enhances")
+if (!require(BiocInstaller)) 
+    source("http://bioconductor.org/biocLite.R")
 
-for (field in fields) {
-    installDeps(field)
+
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+
+installPkg <- function(pkg)
+{
+    lib <- file.path(Sys.getenv("PACKAGEBUILDER_HOME"), "Rlibs")
+    if (!file.exists(lib))
+        dir.create(lib)
+    if (!getOption("pkgType") == "source")
+        tryCatch(biocLite(pkg, lib=lib, suppressUpdates=TRUE),
+            error=biocLite(pkg, type="source", lib=lib,
+            suppressUpdates=TRUE))
+    else
+        biocLite(pkg, lib=lib, suppressUpdates=TRUE)
+}
+
+installDeps <- function(depStr)
+{
+    
+    builtIn <- function(pkg)
+    {
+        pkg %in% c("R", "tools", "utils", "methods", "base", "graphics")
+    }
+    
+    pkgs <- strsplit(depStr, ",", fixed=TRUE)[[1]]
+    for (pkg in pkgs) {
+        pkg <- trim(pkg)
+        if (length(grep("(", pkg, fixed=TRUE))) {
+            versionSpec <- gsub(".*\\((.*?)\\).*","\\1", pkg)
+            segs <- strsplit(versionSpec, " ", fixed=TRUE)
+            operator <- segs[[1]][1]
+            version <- segs[[1]][2]
+            segs <- strsplit(pkg, "(", fixed=TRUE)
+            pkgName <- trim(segs[[1]][1])
+            if (builtIn(pkgName)) next
+            ip <- installed.packages()
+            if (pkgName %in% ip) {
+                installedVersion <- ip[pkgName, "Version"]
+                if (!version >= installedVersion) {
+                    message(paste(pkgName, "version", installedVersion,
+                      "is too old, updating..."))
+                    installPkg(pkgName)
+                }
+            }
+        } else {
+            message(paste("diag:", pkg, "\n"))
+            if (builtIn(pkg)) next
+            if (!pkg %in% installed.packages())
+                installPkg(pkg)
+        }
+    }
 }
 
 
-cat(args, "\n")
+if (exists("Depends")) installDeps(Depends)
+if (exists("Imports")) installDeps(Imports)
+if (exists("Suggests")) installDeps(Suggests)
+if (exists("Enhances")) installDeps(Enhances)
 
-if (!exists("Depends")) {
-    print("'Depends' argument not present.")
-    q("no")
-}
 
