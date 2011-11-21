1bb9c4f8710cded7d6cd8fcb73f7871f386213fa
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-11-21 03:28:21 +0000

    using higher level threading library
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioC/admin/build/packagebuilder@60598 bc3139a8-67e5-0310-9ffc-ced21a209358

--- workers/builder.py
+++ workers/builder.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
 
-## Assume this script is started by a shell script which has read 
+## Assume this script is started by a shell script which has read
 ## BBS variables and also changed to the correct directory.
 
 import os
 import sys
 import json
 import subprocess
-import thread
+import threading
 import time
 import datetime
 import shlex
 import platform
-from stompy import Stomp    
+from stompy import Stomp
 
 
 def send_message(msg, status=None):
@@ -50,7 +50,7 @@ def is_build_required(manifest):
     if ("force" in manifest.keys()):
         if (manifest['force'] == True):
             return(True)
-
+    
     global package_name
     package_name = manifest['job_id'].split("_")[0]
     if (is_svn_package()):
@@ -59,28 +59,28 @@ def is_build_required(manifest):
         print "svn_user ="  + os.getenv("SVN_USER")
         print "svn_pass = " + os.getenv("SVN_PASS")
         try:
-        
-            description = subprocess.Popen(["curl", "-k", "-s", 
+            
+            description = subprocess.Popen(["curl", "-k", "-s",
                 "--user", "%s:%s" % (os.getenv("SVN_USER"), os.getenv("SVN_PASS")),
                 description_url], stdout=subprocess.PIPE).communicate()[0] # todo - handle it if description does not exist
         except:
             print "Unexpected error:", sys.exc_info()[0]
-
+        
         print "debug -- description ="
         print description
         print "description length = %d" % len(description)
-    
+        
         dcf_file = dcf.DcfRecordParser(description.rstrip().split("\n"))
         send_dcf_info(dcf_file)
-    
 
+        
         svn_version = dcf_file.getValue("Version")
     else:
         tmp = manifest["svn_url"].split("/")
         pkgname = tmp[len(tmp)-1].replace(".tar.gz", "")
         svn_version = pkgname.split("_")[1]
-        
             
+    
     bioc_r_map = {"2.7": "2.12", "2.8": "2.13", "2.9": "2.14", "2.10": "2.15"} # need a better way to determine R version
     r_version = bioc_r_map[os.getenv("BBS_BIOC_VERSION")]
     pkg_type = BBScorevars.getNodeSpec(builder_id, "pkgType")
@@ -96,7 +96,7 @@ def is_build_required(manifest):
         base_repo_url = "http://bioconductor.org/course-packages"
     elif (manifest['repository'] == 'scratch'):
         base_repo_url = "http://bioconductor.org/scratch-repos/%s" % manifest['r_version']
-        
+    
     repository_url = "%s/%s/PACKAGES" % (base_repo_url, cran_repo_map[pkg_type])
     # What if there is no file at this url?
     packages = subprocess.Popen(["curl", "-k", "-s", repository_url],
@@ -116,57 +116,56 @@ def is_build_required(manifest):
     return(svn_version != repository_version)
 
 
-def tail2(filename, checking):
-    tail(filename, checking)
 
-def tail(filename, checking):
-    global thread_is_done
-    global message_sequence
-    prevsize = 0
-    if (checking):
-        status = "checking"
-    else:
-        status = "building"
-
-    while 1:
-        time.sleep(0.2)
-        print ("in tail loop")
-        #print ".",
-        if not os.path.isfile(filename):
-            continue
-        st = os.stat(filename)
-        if st.st_size == 0:
-            continue
-        if stop_thread == True:
-            print ("stop_thread == True")
-            num_bytes_to_read = st.st_size - prevsize
-            f = open(filename, 'r')
-            f.seek(prevsize)
-            bytes = f.read(num_bytes_to_read)
-            f.close()
-            print bytes,
-            sys.stdout.flush()
-            send_message({"status": status, "sequence": message_sequence, "body": bytes})
-            prevsize = st.st_size
-            thread_is_done = True
-            print "Thread says I'm done %s" % status
-            thread.exit()
-            break # not needed here but might be needed if program was to continue doing other stuff
-            # and we wanted the thread to exit
-        
-        if (st.st_size > 0):
-            print("st.st_size = %d, prevsize = %s" % (st.st_size, prevsize))
-        if (st.st_size > 0) and (st.st_size > prevsize):
-            num_bytes_to_read = st.st_size - prevsize
-            f = open(filename, 'r')
-            f.seek(prevsize)
-            bytes = f.read(num_bytes_to_read)
-            f.close()
-            print bytes,
-            sys.stdout.flush()
-            send_message({"status": status, "sequence": message_sequence, "body": bytes})
-            message_sequence += 1
-            prevsize = st.st_size
+class Tailer(threading.Thread):
+    def __init(self, filename, checking):
+        threading.Thread.__init__(self)
+        self.filename = filename
+        self.checking = checking
+    def run(self):
+        prevsize = 0
+        if (checking):
+            status = "checking"
+        else:
+            status = "building"
+        while 1:
+            time.sleep(0.2)
+            print ("in tail loop")
+            #print ".",
+            if not os.path.isfile(self.filename):
+                continue
+            st = os.stat(filename)
+            if st.st_size == 0:
+                continue
+            if stop_thread == True:
+                print ("stop_thread == True")
+                num_bytes_to_read = st.st_size - prevsize
+                f = open(filename, 'r')
+                f.seek(prevsize)
+                bytes = f.read(num_bytes_to_read)
+                f.close()
+                print bytes,
+                sys.stdout.flush()
+                send_message({"status": status, "sequence": message_sequence, "body": bytes})
+                prevsize = st.st_size
+                thread_is_done = True
+                print "Thread says I'm done %s" % status
+                break # not needed here but might be needed if program was to continue doing other stuff
+                # and we wanted the thread to exit
+            
+            if (st.st_size > 0):
+                print("st.st_size = %d, prevsize = %s" % (st.st_size, prevsize))
+            if (st.st_size > 0) and (st.st_size > prevsize):
+                num_bytes_to_read = st.st_size - prevsize
+                f = open(self.filename, 'r')
+                f.seek(prevsize)
+                bytes = f.read(num_bytes_to_read)
+                f.close()
+                print bytes,
+                sys.stdout.flush()
+                send_message({"status": status, "sequence": message_sequence, "body": bytes})
+                message_sequence += 1
+                prevsize = st.st_size
 
 
 def setup():
@@ -182,14 +181,14 @@ def setup():
     builder_id = os.getenv("BBS_NODE")
     
     callcount = 1
-
+    
     ## BBS-specific imports
     BBS_home = os.environ['BBS_HOME']
     sys.path.append(BBS_home)
     import BBScorevars
     sys.path.append(os.path.join(BBS_home, "test/python"))
     import dcf
-
+    
     
     packagebuilder_ssh_cmd = BBScorevars.ssh_cmd.replace(os.environ["BBS_RSAKEY"], os.environ["PACKAGEBUILDER_RSAKEY"])
     packagebuilder_rsync_cmd = BBScorevars.rsync_cmd.replace(os.environ["BBS_RSAKEY"], os.environ["PACKAGEBUILDER_RSAKEY"])
@@ -203,7 +202,7 @@ def setup():
         packagebuilder_ssh_cmd = "c:/cygwin/bin/ssh.exe -qi %s -o StrictHostKeyChecking=no" % \
             os.environ["PACKAGEBUILDER_RSAKEY"]
 
-
+    
     print("argument is %s" % sys.argv[1])
     print("cwd is %s" % os.getcwd())
     manifest_fh = open(sys.argv[1], "r")
@@ -227,9 +226,9 @@ def setup_stomp():
         print("Cannot connect")
         raise
     
-    
 
         
+
 
 def svn_export():
     # Don't use BBS_SVN_CMD because it may not be defined on all nodes
@@ -247,7 +246,7 @@ def svn_export():
         "svn export completed with status %d" % retcode})
     if (not retcode == 0):
         sys.exit("svn export failed")
-    
+
 def extract_tarball():
     global package_name
     package_name = manifest['job_id'].split("_")[0]
@@ -258,13 +257,13 @@ def extract_tarball():
 &@action=login' \
 http://tracker.fhcrc.org/roundup/bioc_submit/""" % \
     (os.getenv("TRACKER_LOGIN"), os.getenv("TRACKER_PASSWORD"))
-
+    
     retcode = subprocess.call(cmd, shell=True)
     send_message({"status": "post_processing", "retcode": retcode, "body": \
       "curl to log into tracker returned status %d" % retcode})
     if (not retcode == 0):
         sys.exit("curl to get cookie failed")
-
+    
     retcode = subprocess.call("curl -O -s --cookie cookies.txt %s" % \
         manifest['svn_url'], shell=True)
     send_message({"status": "post_processing", "retcode": retcode, "body": \
@@ -291,7 +290,7 @@ http://tracker.fhcrc.org/roundup/bioc_submit/""" % \
     dcf_file = dcf.DcfRecordParser(description.rstrip().split("\n"))
     send_dcf_info(dcf_file)
     
-    
+
 
 def install_pkg_deps():
     f = open("%s/%s/DESCRIPTION" % (working_dir, package_name))
@@ -314,19 +313,19 @@ def install_pkg_deps():
     send_message({"body": "Result of installing dependencies: %d" % retcode,
       "status": "post_processing", "retcode": retcode})
     return retcode
-    
+
 
 def check_package():
     outfile = "Rcheck.out"
     if (os.path.exists(outfile)):
         os.remove(outfile)
-
+    
     out_fh = open(outfile, "w")
     start_time = datetime.datetime.now()
     stop_thread = False
     thread_is_done = False
     message_sequence = 1
-
+    
     
     pkgname = manifest['job_id'].split("_")[0]
     files = os.listdir(os.getcwd())
@@ -336,19 +335,23 @@ def check_package():
             break
     cmd = "%s CMD check --no-vignettes --timings %s" % (os.getenv('BBS_R_CMD'),
       tarball)
-    thread_id = thread.start_new_thread(tail2,(outfile, True,))
-    print("thread_id in check_package(): %d" % thread_id)
+    background = Tailer(out_fh, True)
+    background.start()
+    #thread_id = thread.start_new_thread(tail2,(outfile, True,))
+    #print("thread_id in check_package(): %d" % thread_id)
     retcode = subprocess.call(cmd, stdout=out_fh, stderr=subprocess.STDOUT, shell=True)
     stop_time = datetime.datetime.now()
     elapsed_time = str(stop_time - start_time)
-    stop_thread = True # tell thread to stop
     out_fh.close()
-    
-    while thread_is_done == False:
-        print ("waiting for thread to tell us to stop...")
-        #pass # wait till thread tells us to stop
-    print "Done checking"
+    stop_thread = True # tell thread to stop
 
+    background.join()
+    
+    #while thread_is_done == False:
+    #    print ("waiting for thread to tell us to stop...")
+    #    #pass # wait till thread tells us to stop
+    #print "Done checking"
+    
     # check for warnings
     out_fh = open(outfile)
     warnings = False
@@ -357,13 +360,13 @@ def check_package():
             warnings = True
             break
     out_fh.close()
-
+    
     send_message({"status": "check_complete", "result_code": retcode, "warnings": warnings,
         "body": "Check completed with status %d" % retcode, "elapsed_time": elapsed_time})
         
-        
-    return (retcode)
     
+    return (retcode)
+
 
 def build_package(): # todo - refactor to allow either source or binary builds
     global stop_thread
@@ -373,22 +376,24 @@ def build_package(): # todo - refactor to allow either source or binary builds
     stop_thread = False
     thread_is_done = False
     message_sequence = 1
+
+
     
-
-
     outfile = "R.out"
     if (os.path.exists(outfile)):
         os.remove(outfile)
     pkg_type = BBScorevars.getNodeSpec(builder_id, "pkgType")
     flags = "--keep-empty-dirs --no-resave-data"
         
-        
+    
     #flags += " --no-vignettes"  ## be sure to comment this line!!!!!!! (used for testing, to speed up builds)
     
     out_fh = open(outfile, "w")
     start_time = datetime.datetime.now()
-    thread_id = thread.start_new_thread(tail,(outfile, False,))
-    print("thread_id in build_package(): %d" % thread_id)
+    #thread_id = thread.start_new_thread(tail,(outfile, False,))
+    #print("thread_id in build_package(): %d" % thread_id)
+    background = Tailer(out_fh, False)
+    background.start()
     
     if (pkg_type == "source"):
         r_cmd = "%s CMD build %s %s" % (os.getenv("BBS_R_CMD"), flags, package_name)
@@ -407,10 +412,11 @@ def build_package(): # todo - refactor to allow either source or binary builds
     elapsed_time = str(stop_time - start_time)
     stop_thread = True # tell thread to stop
     out_fh.close()
-
-    while thread_is_done == False: pass # wait till thread tells us to stop
+    
+    #while thread_is_done == False: pass # wait till thread tells us to stop
+    background.join()
     print "Done"
-
+    
     # check for warnings
     out_fh = open(outfile)
     warnings = False
@@ -419,11 +425,11 @@ def build_package(): # todo - refactor to allow either source or binary builds
             warnings = True
             break
     out_fh.close()
-
+    
     send_message({"status": "build_complete", "result_code": retcode, "warnings": warnings,
         "body": "Build completed with status %d" % retcode, "elapsed_time": elapsed_time})
         
-        
+    
     return (retcode)
 
 def svn_info():
@@ -456,7 +462,7 @@ def propagate_package():
     
     # now install the package
     r_cmd = "%s CMD INSTALL %s" % (os.getenv("BBS_R_CMD"), build_product)
-
+    
     send_message({"body": "Installing package", "status": "post_processing", "retcode": 0})
     
     retcode = subprocess.call(r_cmd, shell=True)
@@ -494,13 +500,13 @@ def propagate_package():
     if (platform.system() == "Windows"):
         retcode = subprocess.call("c:/cygwin/bin/ssh.exe -qi c:/packagebuilder/.packagebuilder.private_key.rsa -o StrictHostKeyChecking=no biocadmin@merlot2.fhcrc.org 'rm -f %s/%s_*.zip'" % (repos, package_name))
     else:
-        retcode = ssh("rm -f %s" % files_to_delete) 
-
+        retcode = ssh("rm -f %s" % files_to_delete)
+    
     print("result of deleting files: %d" % retcode)
     send_message({"body": "Pruning older packages from repository", "status": "post_processing", "retcode": retcode})
     if retcode != 0:
         sys.exit("repos prune failed")
-
+    
     if (platform.system() == "Windows"):
         print("platform.system() == 'Windows', running chmod commands...")
         chmod_retcode = subprocess.call("chmod a+r %s" % os.path.join(working_dir, package_name))
@@ -515,7 +521,7 @@ def propagate_package():
         print("chmod code not run, because platform.system() == %s" % platform.system())
         retcode = scp(build_product, repos)
     
-    print("result of copying file: %d" % retcode) 
+    print("result of copying file: %d" % retcode)
     send_message({"body": "Copied build file to repository", "status": "post_processing", "retcode": retcode,
         "build_product": build_product, "filesize": filesize})
     if retcode != 0:
@@ -550,7 +556,7 @@ def _call(command_str, shell):
 def ssh(command, user='biocadmin', host='merlot2.fhcrc.org'):
     command = "%s %s@%s '%s'" % (packagebuilder_ssh_cmd, user, host, command)
     print("ssh command: %s" % command)
-    retcode = _call([command], shell=True) 
+    retcode = _call([command], shell=True)
     return(retcode)
 
 def scp(src, dest, srcLocal=True, user='biocadmin', host='merlot2.fhcrc.org'):
@@ -590,7 +596,7 @@ def update_packages_file():
         % (packagebuilder_ssh_cmd, script_loc, repos, pkg_type)
     print("update packages command: ")
     print(command)
-    retcode = subprocess.call(command, shell=True) 
+    retcode = subprocess.call(command, shell=True)
     print "retcode for update packages: %d" % retcode
     send_message({"status": "post_processing", "retcode": retcode, "body": "Updated packages list"})
     if retcode != 0:
@@ -605,7 +611,7 @@ def update_packages_file():
             "build_product": build_product, "url": url})
         if retcode != 0:
             sys.exit("Sync to website failed")
-    
+
 
 def get_r_version():
     print("BBS_R_CMD == %s" % os.getenv("BBS_R_CMD"))
@@ -622,7 +628,7 @@ def get_node_info():
     plat = BBScorevars.getNodeSpec(builder_id, "Platform")
     send_message({"status": "node_info", "r_version": r_version,
         "os": os, "arch": arch, "platform": plat, "body": "node_info"})
-    
+
 
 def is_valid_url():
     if (manifest['svn_url'].lower().startswith("https://hedgehog.fhcrc.org")):
@@ -634,26 +640,26 @@ def is_valid_url():
     
     if svn_url:
         description_url = manifest['svn_url'].rstrip("/") + "/DESCRIPTION"
-        description = subprocess.Popen(["curl", "-k", "-s", 
+        description = subprocess.Popen(["curl", "-k", "-s",
             "--user", "%s:%s" % (os.getenv("SVN_USER"), os.getenv("SVN_PASS")),
             description_url], stdout=subprocess.PIPE).communicate()[0]
         if (len(description) == 0  or description.lower().find("404 not found") > -1):
             return False
         return True
     return True
-    
+
 def is_svn_package():
     if (manifest['svn_url'].lower().startswith("https://hedgehog.fhcrc.org")):
         return True
     return False
-    
+
 
 
 ## Main namespace. execution starts here.
 if __name__ == "__main__":
     if (len(sys.argv) < 2):
         sys.exit("builder.py started without manifest file and R version arguments, exiting...")
-
+    
     print "Builder has been started"
     setup()
     setup_stomp()
@@ -665,11 +671,11 @@ if __name__ == "__main__":
         send_message({"status": "invalid_url", "body": "Invalid SVN url."})
         sys.exit(0)
 
-
+    
     get_node_info()
     if is_svn_package():
         svn_info()
-
+    
     
     is_build_required = is_build_required(manifest)
     if not (is_build_required):
@@ -679,16 +685,16 @@ if __name__ == "__main__":
             % pkg_type})
         send_message({"status": "normal_end", "body": "Build process is ending normally."})
         sys.exit(0)
-
+    
     if (is_svn_package()):
         svn_export()
     else:
         extract_tarball()
-        
+    
     result = install_pkg_deps()
     if (result != 0):
         sys.exit(0)
-        
+    
     result = build_package()
     if (result == 0):
         check_result = check_package()
