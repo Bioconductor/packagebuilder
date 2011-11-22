28e6cdd560ca174fac183982e3f1cfeb761dbaa4
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-11-22 18:17:58 +0000

    html debugging
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioC/admin/build/packagebuilder@60718 bc3139a8-67e5-0310-9ffc-ced21a209358

--- node/public/javascripts/packagebuilder.js
+++ node/public/javascripts/packagebuilder.js
@@ -237,11 +237,10 @@ var gotNewNode = function(message) {
     jQuery("#" + nodeName + "_r_version").html(message['r_version']);
     jQuery("#" + nodeName + "_build_id").html(message['job_id']);
 
-    /*
-    if (!gotFirstNode)
+    if (!gotFirstNode) {
         jQuery("#status_header").append(jQuery("#get_header_here").html());
+    }
     gotFirstNode = true;
-    */
 }
 
 
