88d67297531fd7a76eb32e987b8c414929b07b7f
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-10-21 19:06:37 +0000

    upgrade builder to bioc 3.1
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioC/admin/build/packagebuilder@95831 bc3139a8-67e5-0310-9ffc-ced21a209358

--- spb_history/viewhistory/helper.py
+++ spb_history/viewhistory/helper.py
@@ -38,6 +38,12 @@ def filter_out_wrong_versions(builds, job):
 
     nodes = []
 
+    if (r_ver == "3.2"):
+        if bioc_version == "3.1":
+            nodes = ["zin2", "petty", "moscato2", "morelia"]
+        if bioc_version == "3.2":
+            # i assume:
+            nodes = ["zin1", "petty", "moscato1", "oaxaca"]
     if (r_ver == "3.1"):
         if bioc_version == "3.0":
             nodes = ["zin1", "perceval", "moscato1", "oaxaca"]
