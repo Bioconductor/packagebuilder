9dfa03af2d2223898c0331e5d58b7be5c840f2e0
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-08-11 19:24:25 +0000

    change krait to master
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioC/admin/build/packagebuilder@107325 bc3139a8-67e5-0310-9ffc-ced21a209358

--- spb_history/track_build_completion.py
+++ spb_history/track_build_completion.py
@@ -144,11 +144,11 @@ def copy_report_to_site(html, tarball_name):
     ts = time.strftime("%Y%m%d%H%M%S", now)
     destfile = "%s_buildreport_%s.html" % (pkg, ts)
     cmd = \
-      "/usr/bin/scp -i /home/biocadmin/.ssh/pkgbuild_rsa %s webadmin@krait:/extra/www/bioc/spb_reports/%s" % \
+      "/usr/bin/scp -i /home/biocadmin/.ssh/pkgbuild_rsa %s webadmin@master.bioconductor.org:/extra/www/bioc/spb_reports/%s" % \
       (t[1], destfile)
     print("cmd = %s\n" % cmd)
     result = subprocess.call(cmd, shell=True)
-    chmod_cmd = "/usr/bin/ssh -i /home/biocadmin/.ssh/pkgbuild_rsa webadmin@krait \"chmod a+r /extra/www/bioc/spb_reports/%s\"" % destfile
+    chmod_cmd = "/usr/bin/ssh -i /home/biocadmin/.ssh/pkgbuild_rsa webadmin@master.bioconductor.org \"chmod a+r /extra/www/bioc/spb_reports/%s\"" % destfile
     print("chmod_cmd = %s\n" % chmod_cmd)
     result = subprocess.call(chmod_cmd, shell=True)
     os.remove(t[1])
