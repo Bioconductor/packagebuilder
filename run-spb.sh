8e1a008eb2e8123120c7137ea3850973aea8fae4
b-long <b-long@users.noreply.github.com>
2015-12-07 15:37:39 -0500

    Changes that are definitely necessary

--- run-spb.sh
+++ run-spb.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
+
+# TODO: Load variables in a more modular way
+echo "Sourcing unified environment variables"
+. workers/static-config.sh
+
+echo "Now starting server.py ..."
 python -m workers.server > server.log 2>&1 &
+echo "Server is started."
 
-echo "Server should be started ..."
-echo "Now starting archiver"
+echo "Now starting archiver.py ..."
 python -m spb_history.archiver > archiver.log 2>&1 &
-
+echo "Archiver is started."
---
+++ workers/static-config.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# FIXME: This script is based on <BBS git repo>/3.3/config.sh .  Neither system should 
+#         include this informatino by default.  Instead, it should be kept elsewhere
+#         and imported on an as needed basis.
+
+#. ../nodes/$BBS_NODE_HOSTNAME/local-settings.sh
+
+# With StrictHostKeyChecking=no, ssh will automatically add new host keys
+# to the user known hosts files (so it doesn't get stalled waiting for an
+# answer when not run interactively).
+BBS_SSH_CMD="ssh"
+export BBS_SSH_CMD="$BBS_SSH_CMD -qi $BBS_RSAKEY -o StrictHostKeyChecking=no"
+BBS_RSYNC_CMD="rsync"
+export BBS_RSYNC_CMD="$BBS_RSYNC_CMD -rl --delete --exclude='.svn'"
+export BBS_RSYNC_RSH_CMD="$BBS_RSYNC_CMD -e '$BBS_SSH_CMD'"
+
+
+BBS_R_HOME=/home/blong/bin/R-devel
+export BBS_R_CMD="$BBS_R_HOME/bin/R"
+
+
+# Variables specifying the version and mode of the current builds
+# ---------------------------------------------------------------
+
+export BBS_BIOC_VERSION="3.3"
+export BBS_BIOC_VERSIONED_REPO_PATH="$BBS_BIOC_VERSION/$BBS_MODE"
+export BBS_STAGE2_R_SCRIPT="$BBS_HOME/$BBS_BIOC_VERSIONED_REPO_PATH/STAGE2.R"
+export BBS_NON_TARGET_REPOS_FILE="$BBS_HOME/$BBS_BIOC_VERSIONED_REPO_PATH/non_target_repos.txt"
+
+# export BBS_CENTRAL_RHOST="zin2"
+# export BBS_CENTRAL_RUSER="biocbuild"
+# export BBS_CENTRAL_RDIR="/home/biocbuild/public_html/BBS/$BBS_BIOC_VERSIONED_REPO_PATH"
+
+# FIXME: The Single Package Builder (SPB) is failing without this.  Why is it needed?
+export BBS_CENTRAL_BASEURL="http://zin2/BBS/$BBS_BIOC_VERSIONED_REPO_PATH"
+
+
+# 'R CMD check' variables
+# -----------------------
+
+export _R_CHECK_TIMINGS_="0"
+export _R_CHECK_EXECUTABLES_=false
+export _R_CHECK_EXECUTABLES_EXCLUSIONS_=false
