20c5cb3cdb93f19fa56d1bdede544d7e8d338ea7
b-long <b-long@users.noreply.github.com>
2015-12-11 10:09:43 -0500

    Workaround to virtual environment's nounset bug.
    Workaround to https://github.com/pypa/virtualenv/issues/150

--- run-build-server.sh
+++ run-build-server.sh
@@ -7,17 +7,19 @@ err_handler() {
 
 trap 'err_handler $LINENO' ERR
 
+# cd to the scripts current directory
+cd -P -- "$(dirname -- "$0")"
+
+# As a workaround to https://github.com/pypa/virtualenv/issues/150 , we should 
+# enable the virtual environment before setting "nounset"
+echo "Enabling virtual environment"
+source env/bin/activate
+
 # Fail fast (err_handler above will be invoked)
 # Exit immediately if a command exits with a non-zero status.
 set -o errexit
 # Treat unset variables as an error when substituting.
 set -o nounset
-
-# cd to the scripts current directory
-cd -P -- "$(dirname -- "$0")"
-
-echo "Enabling virtual environment"
-source env/bin/activate
 
 # TODO: Load variables in a more modular way
 echo "Sourcing environment variables"
