3651ee961cd1ad03ce21aedbc05b216fc43c2c06
Dan Tenenbaum <dtenenba@fhcrc.org>
2016-01-08 16:53:29 -0800

    on_message() dispatches to a thread so it can return to listening

--- workers/server.py
+++ workers/server.py
@@ -8,6 +8,7 @@ import subprocess
 import uuid
 import stomp
 import logging
+import threading
 # Modules created by Bioconductor
 from bioconductor.config import ENVIR
 from bioconductor.config import TOPICS
@@ -72,71 +73,80 @@ class MyListener(stomp.ConnectionListener):
     def on_error(self, headers, message):
         logging.debug('on_error(): "%s".' % message)
 
+
+
     def on_message(self, headers, body):
+
         logging.info("on_message() Message received")
 
         # Acknowledge that the message has been received
         self.message_received = True
-        logging.info("on_message() Message acknowledged")
+        t = threading.Thread(target=do_work, args=(body,))
+        t.start()
+
+
+def do_work(body):
+    logging.info("on_message() Message acknowledged")
+    try:
+        received_obj = json.loads(body)
+    except ValueError:
+        logging.error("on_message() ValueError: invalid JSON?")
+        return()
+    if ('job_id' in received_obj.keys()): # ignore malformed messages
         try:
-            received_obj = json.loads(body)
-        except ValueError:
-            logging.error("on_message() ValueError: invalid JSON?")
+            job_id = received_obj['job_id']
+            bioc_version = received_obj['bioc_version']
+
+            job_dir = os.path.join(ENVIR['packagebuilder_home'], "jobs")
+            if not os.path.exists(job_dir):
+                os.mkdir(job_dir)
+            job_dir = os.path.join(job_dir, job_id)
+            if not os.path.exists(job_dir):
+                os.mkdir(job_dir)
+            r_libs_dir = os.path.join(job_dir, "R-libs")
+            if not os.path.exists(r_libs_dir):
+                os.mkdir(r_libs_dir)
+            jobfilename = os.path.join(ENVIR['packagebuilder_home'], job_dir,
+                                       "manifest.json")
+
+            jobfile = open(jobfilename, "w")
+            jobfile.write(body)
+            jobfile.close()
+
+            logging.info("on_message() job_dir: '%s'.", job_dir)
+
+            shell_cmd = ["python", "-m", "workers.builder", jobfilename, bioc_version]
+
+            builder_log = open(os.path.join(job_dir, "builder.log"), "w")
+
+            logging.info("on_message() Attempting to run commands from directory: '%s'", os.getcwd())
+            logging.info("on_message() shell_cmd: '%s'", shell_cmd)
+            logging.info("on_message() jobfilename: '%s'", jobfilename)
+            logging.info("on_message() builder_log: '%s'", builder_log)
+            blderProcess = subprocess.Popen(shell_cmd, stdout=builder_log, stderr=builder_log)
+
+            ## TODO - somehow close builder_log filehandle if possible
+            msg_obj = {}
+            msg_obj['builder_id'] = BUILDER_ID
+            msg_obj['body'] = "Got build request..."
+            msg_obj['first_message'] = True
+            msg_obj['job_id'] = job_id
+            msg_obj['client_id'] = received_obj['client_id']
+            msg_obj['bioc_version'] = bioc_version
+            json_str = json.dumps(msg_obj)
+            stomp.send(destination=TOPICS['events'], body=json_str,
+                       headers={"persistent": "true"})
+            logging.info("on_message() Reply sent")
+            blderProcess.wait()
+            logging.info("on_message() blderProcess finished with status {s}.".format(s=blderProcess.returncode))
+            builder_log.close()
+            logging.info("on_message() builder_log closed.")
+        except Exception as e:
+            logging.error("on_message() Caught exception: {e}".format(e=e))
             return()
-        if ('job_id' in received_obj.keys()): # ignore malformed messages
-            try:
-                job_id = received_obj['job_id']
-                bioc_version = received_obj['bioc_version']
+    else:
+        logging.error("on_message() Invalid JSON: missing job_id key.")
 
-                job_dir = os.path.join(ENVIR['packagebuilder_home'], "jobs")
-                if not os.path.exists(job_dir):
-                    os.mkdir(job_dir)
-                job_dir = os.path.join(job_dir, job_id)
-                if not os.path.exists(job_dir):
-                    os.mkdir(job_dir)
-                r_libs_dir = os.path.join(job_dir, "R-libs")
-                if not os.path.exists(r_libs_dir):
-                    os.mkdir(r_libs_dir)
-                jobfilename = os.path.join(ENVIR['packagebuilder_home'], job_dir,
-                                           "manifest.json")
-
-                jobfile = open(jobfilename, "w")
-                jobfile.write(body)
-                jobfile.close()
-
-                logging.info("on_message() job_dir: '%s'.", job_dir)
-
-                shell_cmd = ["python", "-m", "workers.builder", jobfilename, bioc_version]
-
-                builder_log = open(os.path.join(job_dir, "builder.log"), "w")
-
-                logging.info("on_message() Attempting to run commands from directory: '%s'", os.getcwd())
-                logging.info("on_message() shell_cmd: '%s'", shell_cmd)
-                logging.info("on_message() jobfilename: '%s'", jobfilename)
-                logging.info("on_message() builder_log: '%s'", builder_log)
-                blderProcess = subprocess.Popen(shell_cmd, stdout=builder_log, stderr=builder_log)
-
-                ## TODO - somehow close builder_log filehandle if possible
-                msg_obj = {}
-                msg_obj['builder_id'] = BUILDER_ID
-                msg_obj['body'] = "Got build request..."
-                msg_obj['first_message'] = True
-                msg_obj['job_id'] = job_id
-                msg_obj['client_id'] = received_obj['client_id']
-                msg_obj['bioc_version'] = bioc_version
-                json_str = json.dumps(msg_obj)
-                stomp.send(destination=TOPICS['events'], body=json_str,
-                           headers={"persistent": "true"})
-                logging.info("on_message() Reply sent")
-                blderProcess.wait()
-                logging.info("on_message() blderProcess finished with status {s}.".format(s=blderProcess.returncode))
-                builder_log.close()
-                logging.info("on_message() builder_log closed.")
-            except Exception as e:
-                logging.error("on_message() Caught exception: {e}".format(e=e))
-                return()
-        else:
-            logging.error("on_message() Invalid JSON: missing job_id key.")
 
 try:
     logging.debug("Attempting to connect using new communication module")
@@ -150,6 +160,8 @@ except Exception as e:
     raise
 
 logging.info('Waiting for messages; CTRL-C to exit.')
+
+
 
 while True:
     if logging.getLogger().isEnabledFor("debug"):
